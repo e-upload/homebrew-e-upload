79fbef60c973ed9e0518cda119a611aa2119be58
Fy <iofanyer@gmail.com>
2019-05-24 15:25:09 +0800

    Update e-upload.rb

--- e-upload.rb
+++ e-upload.rb
@@ -5,20 +5,15 @@ class EUpload < Formula
   desc "way to tranport easily"
   homepage ""
   url "https://github.com/e-upload/e-upload/releases/download/v0.0.1/e-upload.tar.gz"
-  sha256 "ceaf60d964451a69d34ceb1284a70abaca3311a87606dd1533446aa4ebec3e0f"
+  sha256 "a7d9fb8f690b2b9c3b7a7f60355521ab121fc696877107633a391ee3a6f2f58e"
   # depends_on "cmake" => :build
 
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
     # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
     # system "cmake", ".", *std_cmake_args
     system "make", "install" # if this fails, try separate make/make install steps
     bin.install "e-upload"
-
   end
 
   test do
